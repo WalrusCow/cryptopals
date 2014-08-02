7ec8f9a71c4b31b1f46340e5c24a0f4eb53b43ca
WalrusCow <willgmcd@gmail.com>
2014-08-02 12:08:40 -0700

    Fix pkcs

--- pkcs.py
+++ pkcs.py
@@ -1,21 +1,18 @@
 def pad(byteArray, blockSize):
     ''' Pad bytes fit in given block size. '''
     pad = blockSize - (len(byteArray) % blockSize)
-    if pad == blockSize:
-        return byteArray
     return byteArray + bytes(pad for _ in range(pad))
 
-def getPadding(byteArray, blockSize=16):
-    if not byteArray: return 0
+def getPadding(byteArray):
+    if not byteArray: return None
     b = byteArray[-1]
-    if b >= blockSize: return 0
     l = len(byteArray)
     # Last `b` bytes are all the same
     return b if all(x == b for x in byteArray[l-b:]) else None
 
-def strip(byteArray, blockSize=16):
+def strip(byteArray):
     ''' Strip pkcs padding, or throw if invalid padding. '''
-    b = getPadding(byteArray, blockSize)
+    b = getPadding(byteArray)
     if b is None:
         raise ValueError('Bytes have invalid padding')
     l = len(byteArray)
